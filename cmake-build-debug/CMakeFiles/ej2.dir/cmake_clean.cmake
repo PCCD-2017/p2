e0e935fe6a1f12d36c2a19891341de0ffdd2a2f7
David Rodriguez Pereira <mxmax95@gmail.com>
2017-01-30 19:24:19 +0100

    Fixed CMake (bis).

---
+++ cmake-build-debug/CMakeFiles/ej2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ej2.dir/ej2.c.o"
+  "ej2.pdb"
+  "ej2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ej2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
